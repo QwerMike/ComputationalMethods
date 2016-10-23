c2227d5ca197cda8345a2948dcf1ba0ec272834c
Admin <Admin>
2016-10-23 22:12:12 +0300

    home assignment

---
+++ NewtonMethod/IterativeMethods.cs
@@ -0,0 +1,64 @@
+﻿using MathNet.Symbolics;
+using System;
+using System.Collections.Generic;
+
+namespace NewtonMethod
+{
+    static class IterativeMethods
+    {
+        public static double[] ModifiedNewtonMethod(
+           Expression f, Expression g,
+           double x0, double y0,
+           double precision, out int iterations)
+        {
+            iterations = 0;
+            var x = Expression.Symbol("x");
+            var y = Expression.Symbol("y");
+            double
+                dfx = Calculus.Differentiate(x, f).eval(x0, y0),
+                dfy = Calculus.Differentiate(y, f).eval(x0, y0),
+                dgx = Calculus.Differentiate(x, g).eval(x0, y0),
+                dgy = Calculus.Differentiate(y, g).eval(x0, y0);
+
+            double determinant = dfx * dgy - dfy * dgx;
+            double
+                m11 = (1 / determinant) * dgy, m21 = (-1 / determinant) * dgx,
+                m12 = (-1 / determinant) * dfy, m22 = (1 / determinant) * dfx;
+
+            double xResult, yResult, xPreviuos = x0, yPrevious = y0, difference;
+
+
+            do
+            {
+                ++iterations;
+                xResult = xPreviuos - m11 * f.eval(xPreviuos, yPrevious)
+                    - m12 * g.eval(xPreviuos, yPrevious);
+                yResult = yPrevious - m21 * f.eval(xPreviuos, yPrevious)
+                    - m22 * g.eval(xPreviuos, yPrevious);
+                difference = distance(xPreviuos, yPrevious, xResult, yResult);
+                xPreviuos = xResult;
+                yPrevious = yResult;
+            } while (difference > precision);
+
+            return new double[] { xResult, yResult };
+        }
+
+        public static double eval(this Expression expr, double x, double y)
+        {
+            var variables = new Dictionary<string, FloatingPoint>
+                {
+                    { "x", x },
+                    { "y", y }
+                };
+            return Evaluate.Evaluate(variables, expr).RealValue;
+        }
+
+        private static double distance(
+            double x1, double y1,
+            double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+        }
+
+    }
+}
---
+++ NewtonMethod/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using MathNet.Symbolics;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewtonMethod
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            txtPrecision.Text = "0.001";
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var f = Infix.ParseOrThrow(txtF.Text);
+            var g = Infix.ParseOrThrow(txtG.Text);
+            double x0 = Convert.ToDouble(txtX0.Text, CultureInfo.InvariantCulture);
+            double y0 = Convert.ToDouble(txtY0.Text, CultureInfo.InvariantCulture);
+            double precision = Convert.ToDouble(txtPrecision.Text, CultureInfo.InvariantCulture);
+            int iterations;
+            var result = IterativeMethods.ModifiedNewtonMethod(
+                f, g, x0, y0, precision, out iterations);
+            txtResult.Text =
+                "x: " + result[0] +
+                "\ny: " + result[1] +
+                "\niterations: " + iterations;
+        }
+    }
+}
